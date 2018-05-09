5cf44a5aa72c49ad2d55e893c34ed75102176e5a
Dylan Sheffer <dsheffer16@gmail.com>
2018-05-09 14:21:12 -0400

    additional fields and editorial publishing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          <figure className="image" style={{ margin: '0' }}>
+            <img src={logo} alt="Swift Kick Logo" style={{ width: '150px' }} />
           </figure>
         </Link>
       </div>
