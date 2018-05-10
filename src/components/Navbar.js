d97ea53f1eaee2d79ee416098750787f69dab1ab
Dylan Sheffer <dsheffer16@gmail.com>
2018-05-10 15:48:45 -0400

    Navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,71 +2,116 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faHome, faListUl, faBriefcase, faFile, faUsers, faVideo }  from '@fortawesome/fontawesome-free-solid'
+import { faHome, faListUl, faBriefcase, faFile, faUsers, faVideo, faBars }  from '@fortawesome/fontawesome-free-solid'
 
+const MinNavHeight = "80px";
 
 const Navigation = styled.nav`
-  display: flex;
   background: black;
-  ul {
-    width: 100%;
+  width: 100%;
+  
+  .nav-container {
+    min-height: ${MinNavHeight};
+    max-width: 1024px;
+    margin: 0 auto;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+  }
+
+  .hamburger-container {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    height: ${MinNavHeight};
+    button {
+      background: none;
+      border: none;
+      color: white;
+      padding: 20px;
+    }
+  }
+  #menu {
+    margin: 0;
+    padding: 0;
+    display: block;
     li {
       list-style: none;
+      display: flex;
+      width: 100%;
+      padding: 10px 0;
+      justify-content: center;
       a {
         color: white;
         text-decoration: none;
-        margin: 10px;
-        padding: 10px;
-        svg {
-          // width: 1.125em !important;
-        }
       }
     }
   }
-`
+  #menu.closed {
+    display: none;
+  }
+
+  @media only screen and (min-width: 650px) {
+    .hamburger-container {
+      display: none;
+    }
+    #menu.closed {
+      display: flex;
+    }
+    #menu {
+      margin: auto 0;
+    }
+  }
+  `
+  
+  
+let menuClosed = true;
 
 const Navbar = () => (
   <Navigation>
-    <ul>
-      <li>
-        <Link to="/">
-          <FontAwesomeIcon icon={faHome} size="sm" /> Home
-          {/* <i className="fas fa-home"></i> Home */}
-        </Link>
-      </li>
-      <li>
-        <Link to="/courses">
-          <FontAwesomeIcon icon={faListUl} size="sm" /> Courses
-          {/* <i className="fas fa-list-ul"></i> Courses */}
-        </Link>
-      </li>
-      <li>
-        <Link to="/consulting">
-          <FontAwesomeIcon icon={faBriefcase} size="sm" /> Consulting
-          {/* <i className="fas fa-briefcase"></i> Consulting */}
-        </Link>
-      </li>
-      <li>
-        <Link to="/articles">
-          <FontAwesomeIcon icon={faFile} size="sm" /> Articles
-          {/* <i className="fas fa-file"></i> Articles */}
-        </Link>
-      </li>
-      <li>
-        <Link to="/webinars">
-          <FontAwesomeIcon icon={faVideo} size="sm" /> Webinars
-          {/* <i className="fas fa-video"></i> Webinars */}
-        </Link>
-      </li>
-      <li>
-        <Link to="/about">
-          <FontAwesomeIcon icon={faUsers} size="sm" /> About Us
-          {/* <i className="fas fa-users"></i> About Us */}
-        </Link>
-      </li>
-    </ul>
+    <div className="nav-container">
+      <div className="hamburger-container">
+        <button onClick={() => {
+          const menu = document.querySelector("#menu");
+          menuClosed = !menuClosed;
+          console.log(menu);
+          menuClosed ? menu.classList.add("closed") : menu.classList.remove("closed");
+          }}>
+          <FontAwesomeIcon icon={faBars} size="2x" />
+        </button>
+      </div>
+      <ul id="menu" className="closed">
+        <li>
+          <Link to="/">
+            <FontAwesomeIcon icon={faHome} /> Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/courses">
+            <FontAwesomeIcon icon={faListUl} /> Courses
+          </Link>
+        </li>
+        <li>
+          <Link to="/consulting">
+            <FontAwesomeIcon icon={faBriefcase} /> Consulting
+          </Link>
+        </li>
+        <li>
+          <Link to="/articles">
+            <FontAwesomeIcon icon={faFile} /> Articles
+          </Link>
+        </li>
+        <li>
+          <Link to="/webinars">
+            <FontAwesomeIcon icon={faVideo} /> Webinars
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <FontAwesomeIcon icon={faUsers} /> About Us
+          </Link>
+        </li>
+      </ul>
+    </div>
   </Navigation>
 )
 
