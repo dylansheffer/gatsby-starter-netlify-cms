a620b85619825c7dce84bb75705ad93366ed5979
Dylan Sheffer <dsheffer16@gmail.com>
2018-05-11 19:04:33 -0400

    Added global style to article preview as test

--- src/cms/preview-templates/ArticlePreview.js
+++ src/cms/preview-templates/ArticlePreview.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { ArticleTemplate } from '../../templates/article'
+import styled from 'styled-components'
+
+// TODO: Put Global Styles into its own component that can be imported in
+const GlobalStyles = styled.div`
+  font-family: ${text.sansSerif};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    max-width: 100%;
+  }
+
+  .container {
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+`
 
 const ArticlePreview = ({ entry, widgetFor }) => (
-  <ArticleTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
+  <GlobalStyles>
+    <ArticleTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={entry.getIn(['data', 'tags'])}
+      title={entry.getIn(['data', 'title'])}
+    />
+  </GlobalStyles>
 )
 
 export default ArticlePreview
