9b1a27b6eca4b6d1edd31d24d08902e948cc4ed8
Dylan Sheffer <dsheffer16@gmail.com>
2018-06-06 16:29:05 -0400

    Utilized inject global for global styles and added a normalize

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import styled from 'styled-components'
+import styledNormalize from 'styled-normalize'
+import styled, { injectGlobal } from 'styled-components'
 import fontawesome from '@fortawesome/fontawesome'
 import 'normalize.css'
 import '@fortawesome/fontawesome/styles.css'
@@ -15,60 +16,11 @@ fontawesome.config = {
 	autoAddCss: false
 };
 
-const GlobalStyles = styled.div`
-  font-family: ${text.sansSerif};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: ${colors.gray};
-
-  img {
-    max-width: 100%;
-  }
-
-  h1 {
-    font-size: 40px;
-    text-align: center;
-  }
-
-  h2 {
-    font-size: 24px;
-  }
-
-  h3 {
-    text-align: center;
-    text-transform: uppercase;
-    font-size: 16px;
-    font-weight: 600;
-    letter-spacing: .1em;
-    color: ${colors.lightGray};
-  }
-
-  p {
-    font-size: 16px;
-  }
-
-  a {
-    color: ${colors.red};
-    :hover {
-      text-decoration: underline !important;
-      color: ${colors.lightRed};
-    }
-  }
-
-  .container {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-
-  .page-header, h1, h2 {
-    color: ${colors.gray};
-    font-weight: 500;
-  }
+const Root = styled.div`
 `
 
 const TemplateWrapper = ({ children }) => (
-  <GlobalStyles>
+  <Root>
     <Helmet>
       <title>Swift Kick</title>
       <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css' />
@@ -77,7 +29,60 @@ const TemplateWrapper = ({ children }) => (
     <Hero />
     <div>{children()}</div>
     <Footer />
-  </GlobalStyles>
+  </Root>
 )
+
+injectGlobal`
+${styledNormalize}
+
+body {
+  font-family: ${text.sansSerif};
+  color: ${colors.gray};
+}
+
+img {
+  max-width: 100%;
+}
+
+h1 {
+  font-size: 40px;
+  text-align: center;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+h3 {
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: .1em;
+  color: ${colors.lightGray};
+}
+
+p {
+  font-size: 16px;
+}
+
+a {
+  color: ${colors.red};
+  :hover {
+    text-decoration: underline !important;
+    color: ${colors.lightRed};
+  }
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.page-header, h1, h2 {
+  color: ${colors.gray};
+  font-weight: 500;
+}
+`
 
 export default TemplateWrapper
