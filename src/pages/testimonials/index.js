bcc7ca364ac554791b74d5c9e2816ac08332ae99
Dylan Sheffer <dsheffer16@gmail.com>
2018-06-11 14:14:07 -0400

    testimonial

--- src/pages/testimonials/index.js
+++ src/pages/testimonials/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 import Container from '../../components/Container';
 import Testimonial from '../../components/Testimonial';
 import FlexContainer from '../../components/FlexContainer'
 
+const TestimonialConainer = styled(Container)`
+  margin: 25px auto;
+`
+
 const TestimonialPage = ({ data }) => (
-  <Container>
-        <h1 className="page-header">Testimonials</h1>
-        <FlexContainer>
-        {data.allMarkdownRemark.edges
-            .filter(testimonial => testimonial.node.frontmatter.templateKey == 'testimonial')
-            .map(({ node: testimonial }) => (
-            <Testimonial key={testimonial.id} data={testimonial} />
-          ))}
-          </FlexContainer>
-    </Container>
+  <TestimonialConainer>
+    <Link className="bread-crumb" to="/">Back to Home</Link>
+    <h1 className="page-header">Testimonials</h1>
+    <FlexContainer>
+    {data.allMarkdownRemark.edges
+        .filter(testimonial => testimonial.node.frontmatter.templateKey == 'testimonial')
+        .map(({ node: testimonial }) => (
+        <Testimonial key={testimonial.id} data={testimonial} />
+      ))}
+      </FlexContainer>
+  </TestimonialConainer>
 )
 
 export const pageQuery = graphql`
