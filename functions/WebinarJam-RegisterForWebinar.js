ac36aa31eae1f7e40b3dc5de78a2dcf0d75e3210
Dylan Sheffer <dsheffer16@gmail.com>
2018-07-03 10:16:01 -0400

    parse

--- functions/WebinarJam-RegisterForWebinar.js
+++ functions/WebinarJam-RegisterForWebinar.js
@@ -5,9 +5,9 @@ exports.handler = function(event, context, callback) {
         lastName,
         email,
         schedule
-    } = JSON.stringify(event.body);
+    } = JSON.parse(event.body);
 
-    console.log(event);
+    console.log(JSON.parse(event.body));
 
     const data = {
         apiKey: process.env.WEBINAR_JAM_API_KEY || '',
